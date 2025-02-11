25e2a5f0d613b42a6c3ebe4d1ff24cc76b7073da
Panagiotis Giannoumis <panagiotis.giannoumis@sportserve.co>
2025-02-11 20:50:56 +0200

    Revert "Add a dynamin reactImg image in App.jsx"

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -1,8 +1,14 @@
+const reactDescriptions = ['Fundamental', 'Crusial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random()*(max + 1));
+}
+
 function Header() {
   
   return (
     <header>
-      <img src={reactImg} alt="Stylized atom" />
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         Fundamental React concepts you will need for almost any app you are
