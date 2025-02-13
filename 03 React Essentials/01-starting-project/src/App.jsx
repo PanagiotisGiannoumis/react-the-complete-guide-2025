d8d25fe9a5d65baa3c16c0a901ebb198dd726463
Panagiotis Giannoumis <panagiotis.giannoumis@sportserve.co>
2025-02-13 11:16:39 +0200

    Added components folder and made standalone component files

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -1,54 +1,8 @@
-// Import an image from assets folder
-import reactImg from './assets/react-core-concepts.png';
-
 // Import CORE_CONCEPTS array from data.js
 import { CORE_CONCEPTS } from './data.js';
 
-// Create an array
-const reactDescriptions = ['Fundamental', 'Crusial', 'Core'];
-
-// Make a function to randomly generate a number between 0-2
-function genRandomInt(max) {
-  return Math.floor(Math.random()*(max + 1));
-}
-
-// Header Component
-function Header() {
-  // Create a variable named description passing that random index to it
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-// Alternative way tocreate CoreConcept Component
-// function CoreConcept(props) {
-//   return(
-//     <li>
-//       <img src={props.image} alt={props.title} />
-//       <h3>{props.title}</h3>
-//       <p>{props.description}</p>
-//     </li>
-//   );
-// }
-
-// CoreConcept Component
-function CoreConcept({title, image, description}) {
-  return(
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreCocept.jsx';
 
 // App Component
 function App() {
---
+++ 03 React Essentials/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,26 @@
+// Import an image from assets folder
+import reactImg from '../assets/react-core-concepts.png';
+
+// Create an array
+const reactDescriptions = ['Fundamental', 'Crusial', 'Core'];
+
+// Make a function to randomly generate a number between 0-2
+function genRandomInt(max) {
+  return Math.floor(Math.random()*(max + 1));
+}
+
+// Header Component
+export default function Header() {
+    // Create a variable named description passing that random index to it
+    const description = reactDescriptions[genRandomInt(2)];
+    return (
+      <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    );
+  }
